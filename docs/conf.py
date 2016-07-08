8e463a801aa86ab7d42f9819189ad775665bdb62
nils <nils.haentjens@maine.edu>
2016-07-08 16:18:25 -0400

    Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Inlinino'
-copyright = u'2016, Nils'
+copyright = u'2016, Inlinino'
 author = u'Nils'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -114,7 +114,8 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
