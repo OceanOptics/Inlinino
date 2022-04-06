a2a4145c7e230e271b9c575bbf4a03d3524caf3d
cathmmitchell <40070421+cathmmitchell@users.noreply.github.com>
2022-04-06 15:04:13 -0400

    Update hyperbb.py to log bb and raw

--- inlinino/instruments/hyperbb.py
+++ inlinino/instruments/hyperbb.py
@@ -111,7 +111,8 @@ class HyperBB(Instrument):
         self._plot_curve.setData(self._parser.wavelength, self.signal_reconstructed)
         # Log data as received
         if self.log_prod_enabled and self._log_active:
-            self._log_prod.write(bb, timestamp)
+            raw_bb = np.concatenate((raw,bb),axis=1)
+            self._log_prod.write(raw_bb, timestamp)
             if not self.log_raw_enabled:
                 self.signal.packet_logged.emit()
 
@@ -137,11 +138,11 @@ class MetaHyperBBParser(type):
         cls.FRAME_VARIABLES = ['ScanIdx', 'DataIdx', 'Date', 'Time', 'StepPos', 'wl', 'LedPwr', 'PmtGain', 'NetSig1',
                                'SigOn1', 'SigOn1Std', 'RefOn', 'RefOnStd', 'SigOff1', 'SigOff1Std', 'RefOff',
                                'RefOffStd', 'SigOn2', 'SigOn2Std', 'SigOn3', 'SigOn3Std', 'SigOff2', 'SigOff2Std',
-                               'SigOff3', 'SigOff3Std', 'LedTemp', 'WaterTemp', 'Depth', 'Debug1', 'zDistance']
+                               'SigOff3', 'SigOff3Std', 'LedTemp', 'WaterTemp', 'Depth', 'Debug1', 'zDistance', 'bb']
         cls.FRAME_TYPES = [int, int, str, str, int, int, int, int, int,
                            float, float, float, float, float, float, float,
                            float, float, float, float, float, float, float,
-                           float, float, float, float, float, int, int]
+                           float, float, float, float, float, int, int, float]
         # FRAME_PRECISIONS = ['%d', '%d', '%s', '%s', '%d', '%d', '%d', '%d', '%d',
         #                    '%.1f', '%.1f', '%.1f', '%.1f', '%.1f', '%.1f', '%.1f',
         #                    '%.1f', '%.1f', '%.1f', '%.1f', '%.1f', '%.1f', '%.1f',
@@ -290,8 +291,8 @@ class HyperBBParser(metaclass=MetaHyperBBParser):
 
 
 if __name__ == "__main__":
-    p_cal = '/Users/nils/Documents/Lab/Inlinino/development_ressources/HBB_SN8005_ShipDisk-2/MatlabProcessingSoftware/Hbb_Cal_Plaque_20210315_114120.mat'
-    t_cal = '/Users/nils/Documents/Lab/Inlinino/development_ressources/HBB_SN8005_ShipDisk-2/MatlabProcessingSoftware/HBB_Cal_Temp_20210315_205506.mat'
+    p_cal = 'C:/Users/BalchLab/Desktop/HyperBB/Calibrations/Hbb_Cal_Plaque_20220317_115328.mat'
+    t_cal = 'C:/Users/BalchLab/Desktop/HyperBB/Calibrations/HBB_Cal_Temp_20220315_162735.mat'
 
     hbb = HyperBBParser(p_cal, t_cal)
 
