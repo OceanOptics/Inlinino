09e5c4713037b4ceeca5d835fe5f54b8e8db40c7
Chan Lee <chan.lee@umbrage.com>
2023-04-25 21:26:22 +0000

    adds error handling to calibrate dialog

--- inlinino/widgets/hypernav/calibrate_dialog.py
+++ inlinino/widgets/hypernav/calibrate_dialog.py
@@ -1,13 +1,13 @@
 import os.path
 import queue
+import re
 from threading import Thread
 from multiprocessing import Process, Queue
 
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets, uic
 from functools import reduce, partial
 
-# TODO move read_manufacturer_pixel_registration to io as static function
-from inlinino.instruments.hypernav import HyperNav, read_manufacturer_pixel_registration
+from inlinino.instruments.hypernav import HyperNav
 from inlinino.widgets.shared.file_label import FileLabel
 from inlinino import PATH_TO_RESOURCES
 
@@ -28,8 +28,8 @@ class HyperNavCalibrateDialogWidget(QtWidgets.QDialog):
 
         self.log_file_label.setText(log_file_name)
 
-        self.run_button = self.button_box.addButton("Run", QtGui.QDialogButtonBox.AcceptRole)
-        self.cancel_button = self.button_box.addButton("Cancel", QtGui.QDialogButtonBox.RejectRole)
+        self.run_button = self.button_box.addButton("Run", QtGui.QDialogButtonBox.ActionRole)
+        self.cancel_button = self.button_box.addButton("Close", QtGui.QDialogButtonBox.RejectRole)
         self.run_button.clicked.connect(self.start_clicked)
         self.cancel_button.clicked.connect(self.cancel_clicked)
         self.run_button.setEnabled(False)
@@ -107,43 +107,53 @@ class HyperNavCalibrateDialogWidget(QtWidgets.QDialog):
     ):
         hn = HyperNavIO(sn=hn_sn, prt_head_sn=prt_sn, stb_head_sn=sbd_sn)
         data, meta = hn.read_inlinino(os.path.join(path, filename))
-        # Find HyperNav frame serial numbers
-        analyzed_sn = set([int(k[6:]) for k in meta['valid_frames'].keys() if
-                            True in [k.startswith(hdr) for hdr in ('SATYLZ', 'SATYDZ', 'SATXLZ', 'SATXDZ')]])
+        light_sn_list = list(hdr for hdr in meta['valid_frames'].keys() if re.match("^SAT[XY]LZ.*", hdr))
+        dark_sn_list = list(hdr for hdr in meta['valid_frames'].keys() if re.match("^SAT[XY]DZ.*", hdr))
 
-        wl_data = read_manufacturer_pixel_registration(wavelength_file_path)
+        # Find HyperNav frame serial numbers
+
+        if not light_sn_list:
+            queue.put(('error', 'no valid light frames '))
+            return
+
+        if not dark_sn_list:
+            queue.put(('error', 'no valid dark frames'))
+            return
+
+        light_sn = light_sn_list[0]
+        dark_sn = dark_sn_list[0]
 
         warning_sn = []
-        for sn in analyzed_sn:
+        for sn in set([int(k[6:]) for k in [ light_sn, dark_sn ]]):
             if sn not in (prt_sn, sbd_sn):
                 warning_sn.append(sn)
-            calibration_report(
-                sn,
-                data,
-                lamp_file_path,
-                plaque_file_path,
-                wl_data,
-                fel_lamp_calibration_distance_meters,
-                fel_lamp_to_plaque_distance_meters
-            )
         if warning_sn:
-            queue.put((filename, 'warning', ', '.join([f'{sn:04d}' for sn in warning_sn])))
-        else:
-            queue.put((filename, 'ok'))
+            queue.put(('warning', 'Unexpected head serial number(s) ' + ', '.join(warning_sn)))
+
+        warnings = calibration_report(
+            data[light_sn],
+            data[dark_sn],
+            lamp_file_path,
+            plaque_file_path,
+            wavelength_file_path,
+            fel_lamp_calibration_distance_meters,
+            fel_lamp_to_plaque_distance_meters
+        )
+
+        for warning in warnings:
+            queue.put(('warning', warning))
 
     def _join(self):
         if self.worker is None:
             return
         self.worker.join()
-        # Check if job finished properly
-        try:
-            result = self.queue.get_nowait()
-        except queue.Empty:
-            result = ('error', 'No results from worker.')
-        if result[1] == 'warning':
-            self.instrument.signal.warning.emit(f'Unexpected head serial number(s) {result[2]} in "{result[0]}".')
-        elif result[1] == 'error':
-            self.instrument.signal.warning.emit(f'The error "{result[2]}" occurred while analyzing "{result[0]}".\n'
-                                                f'Unable to generate report.')
+        while not self.queue.empty():
+            level, message = self.queue.get_nowait()
+            if level == 'warning':
+                self.instrument.signal.warning.emit(f'Warning while analyzing "{self.log_file_name}"\n\n{message}')
+            elif level == 'error':
+                self.instrument.signal.warning.emit(f'The error "{message}" occurred while analyzing "{self.log_file_name}".\n\nUnable to generate report.')
+
         # Reset buttons
         self.run_button.setEnabled(True)
+        self.run_button.setText('Run')
